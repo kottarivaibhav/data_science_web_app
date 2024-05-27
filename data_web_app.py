77c97812a2b7aae683f2ddd7f83bde5b9820b1ed
Vaibhav Ramachandra Kottari <114846137+kottarivaibhav@users.noreply.github.com>
2024-05-27 11:46:11 +0530

    Update data_web_app.py
    completely updated the whole code

--- data_web_app.py
+++ data_web_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import requests
 
 # Load data from the API endpoint
 DATA_URL = "https://data.cityofnewyork.us/resource/h9gi-nx95.json"
@@ -24,26 +25,26 @@ data = load_data()
 # Sidebar for filter options
 st.sidebar.title("Filter Options")
 hour = st.sidebar.slider("Select Hour of Day", 0, 23, 12)
-modes = st.sidebar.multiselect("Select Mode(s) of Transportation", data['VEHICLE_TYPE_1'].unique())
+modes = st.sidebar.multiselect("Select Mode(s) of Transportation", data['vehicle_type_code1'].unique())
 
 # Filter data
-filtered_data = data[data['CRASH_DATE_CRASH_TIME'].dt.hour == hour]
+filtered_data = data[data['CRASH DATE & TIME'].dt.hour == hour]
 if modes:
-    filtered_data = filtered_data[filtered_data['VEHICLE_TYPE_1'].isin(modes)]
+    filtered_data = filtered_data[filtered_data['vehicle_type_code1'].isin(modes)]
 
 # Map view of accidents
 st.title("Motor Vehicle Collisions in NYC")
-st.map(filtered_data)
+st.map(filtered_data[['latitude', 'longitude']])
 
 # Time distribution of accidents
 st.subheader("Accidents by Hour of Day")
-hourly_distribution = filtered_data.groupby(filtered_data['CRASH_DATE_CRASH_TIME'].dt.hour).size()
+hourly_distribution = filtered_data.groupby(filtered_data['CRASH DATE & TIME'].dt.hour).size()
 st.bar_chart(hourly_distribution)
 
 # Mode of transportation breakdown
 if modes:
     st.subheader("Accidents by Mode of Transportation")
-    mode_distribution = filtered_data['VEHICLE_TYPE_1'].value_counts()
+    mode_distribution = filtered_data['vehicle_type_code1'].value_counts()
     st.bar_chart(mode_distribution)
 
 # Raw data
